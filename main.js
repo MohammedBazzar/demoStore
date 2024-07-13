af9e0a845115954c13231c8c58030e55f625b93e
MohammedBazzar <momobazzar12@gmail.com>
2024-07-14 00:01:57 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("May chaos take the World");
+const  getProduct = async()=>{
+    const {data} = await axios.get('https://dummyjson.com/products');
+    return data;
+}
+const displayProducts = async() =>{
+const data = await getProduct();
+const products = data.products
+const result = data.products.map(function(p){
+    return `
+    <a href="./details.html?id=${p.id}">
+    <div class="product">
+    <h2>${p.title}</h2>
+    <img src="${p.thumbnail}" />
+    <h3>${Math.round(p.price)}$</h3></a>
+    
+
+    </div>`
+}).join(" ");
+document.querySelector(".products").innerHTML=result;
+
+}
+displayProducts();
